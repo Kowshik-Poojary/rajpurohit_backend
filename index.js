845159c44056c5bc5c4b6b53bf226410618ee41f
Kowshik-Poojary <chandrapoojary1975@gmail.com>
2025-07-05 17:49:34 +0530

    adding connection pool

--- index.js
+++ index.js
@@ -10,25 +10,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-// MySQL connection
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
-
-
-
-db.connect(err => {
-  if (err) {
-    console.error("Database connection failed:", err);
-    return;
-  }
-  console.log("Connected to MySQL database");
-});
-
 // ✅ Test route to verify server
 app.get("/test", (req, res) => {
   res.send("API is working ✅");
